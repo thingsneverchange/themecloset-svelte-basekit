3d25ddf4cebb589ac072ffad99ffce63ba1a4ec4
Yongju Lee <lee@yongju.me>
2022-04-19 12:18:38 +0900

    added new set of icons

--- src/lib/ui/Wrapper.svelte
+++ src/lib/ui/Wrapper.svelte
@@ -5,9 +5,11 @@ export let spacingTop: number = 0;
 export let spacingLeft: number = 0;
 export let spacingRight: number = 0;
 export let spacingBottom: number = 0;
+export let align: string = 'left'
 </script>
 
 <div style="padding:{spacingHorizontal}px {spacing}px;
+            text-align:{align};
             {spacingTop != 0 ? 'padding-top:' + spacingTop.toString() + "px;" : ''}
             {spacingLeft != 0 ? 'padding-left:' + spacingLeft.toString() + "px;" : ''}
             {spacingRight != 0 ? 'padding-right:' + spacingRight.toString() + "px;" : ''}
