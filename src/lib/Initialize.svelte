65e6dd151db7010731596887de8616f8a5e91d5d
Yongju Lee <lee@yongju.me>
2022-04-20 17:39:35 +0900

    Changed store

--- src/lib/Initialize.svelte
+++ src/lib/Initialize.svelte
@@ -1,7 +1,9 @@
 <script type="ts">
+export let fontFamily: string = 'arial'
+export let fontSize: string  = '10pt'
 import './assets/css/global.css'
 import './assets/css/reset.css'
 </script>
-<div id="themecloset_app" style="font-family: {scan().fontFamily};font-size: {scan().fontSize}{scan().fontSizeUnit}">
+<div id="themecloset_app" style="font-family: {fontFamily};font-size: {fontSize}">
   <slot />
 </div>
