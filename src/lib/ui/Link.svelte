8ca0bb279553d5acf0fbb62a5ab06a219ed8988b
Yongju Lee <lee@yongju.me>
2022-04-21 01:01:41 +0900

    Changed store

--- src/lib/ui/Link.svelte
+++ src/lib/ui/Link.svelte
@@ -1,10 +1,13 @@
 <script type="ts">
 import { goto } from '$app/navigation'
+import { createEventDispatcher } from 'svelte';
+const dispatch = createEventDispatcher();
  export let href : string;
  export let target: string = undefined;
 
  const sendLink = (event: MouseEvent) : void => {
    event.preventDefault()
+   dispatch('click')
    if(href){
      goto(href)
    }
