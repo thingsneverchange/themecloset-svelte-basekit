d9ba8eabec2f24d75756cae6ee8580db9a063b98
Yongju Lee <lee@yongju.me>
2022-04-20 15:31:59 +0900

    New select

--- src/lib/ui/Link.svelte
+++ src/lib/ui/Link.svelte
@@ -1,7 +1,7 @@
 <script type="ts">
 import { goto } from '$app/navigation'
  export let href : string;
- export let target: string;
+ export let target: string = undefined;
 
  const sendLink = (event: MouseEvent) : void => {
    event.preventDefault()
