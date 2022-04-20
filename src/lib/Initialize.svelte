e154c3a015919fb2af66e572f154eb03bfdd5996
Yongju Lee <lee@yongju.me>
2022-04-20 17:21:19 +0900

    Changed store

--- src/lib/Initialize.svelte
+++ src/lib/Initialize.svelte
@@ -2,7 +2,6 @@
 import './assets/css/global.css'
 import './assets/css/reset.css'
 import { scan } from '$lib/store'
-import Notification from '$lib/ui/Notification.svelte'
 </script>
 <div id="themecloset_app" style="font-family: {scan().fontFamily};font-size: {scan().fontSize}{scan().fontSizeUnit}">
   <slot />
