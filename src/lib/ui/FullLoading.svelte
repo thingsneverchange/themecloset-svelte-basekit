f32441296159599f753a7588a6b5df352e88e56d
Yongju Lee <lee@yongju.me>
2022-04-21 23:26:31 +0900

    Changed store

---
+++ src/lib/ui/FullLoading.svelte
@@ -0,0 +1,76 @@
+<div class="loadingTooltip">
+  <div class="loader">Loading...</div>
+</div>
+<div class="uploadingOverlay">
+</div>
+
+<style>
+.uploadingOverlay{position:fixed;top:0px;left:0px;width:100%;height:100%;background-color:rgba(0,0,0,0.3);z-index:9999}
+
+.loadingTooltip{display:flex;justify-content: center;align-items: center}
+.loadingTooltip{font-size:10pt;width:120px;height:40px;background-color:rgba(0,0,0,0.1);z-index:10000;border-radius:5px;position:fixed;top:50%;left:50%;margin-top:-20px;margin-left:-60px;}
+.loader {
+  font-size: 10px;
+  margin-left:10px;
+  text-indent: -9999em;
+  width: 15px;
+  height: 15px;
+  border-radius: 50%;
+  background: #ffffff;
+  background: -moz-linear-gradient(left, #ffffff 10%, rgba(255, 255, 255, 0) 42%);
+  background: -webkit-linear-gradient(left, #ffffff 10%, rgba(255, 255, 255, 0) 42%);
+  background: -o-linear-gradient(left, #ffffff 10%, rgba(255, 255, 255, 0) 42%);
+  background: -ms-linear-gradient(left, #ffffff 10%, rgba(255, 255, 255, 0) 42%);
+  background: linear-gradient(to right, #ffffff 10%, rgba(255, 255, 255, 0) 42%);
+  position: relative;
+  -webkit-animation: load3 1.4s infinite linear;
+  animation: load3 1.4s infinite linear;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+}
+.loader:before {
+  width: 50%;
+  height: 50%;
+  background: #ffffff;
+  border-radius: 100% 0 0 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  content: '';
+}
+.loader:after {
+  background: rgba(0,0,0.0);
+  width: 75%;
+  height: 75%;
+  border-radius: 50%;
+  content: '';
+  margin: auto;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+}
+@-webkit-keyframes load3 {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@keyframes load3 {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+
+</style>
