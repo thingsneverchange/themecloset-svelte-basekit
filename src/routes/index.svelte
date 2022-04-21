ce6dca0237ea99848464ddfe613f18f40b55d0ca
Yongju Lee <lee@yongju.me>
2022-04-21 15:32:44 +0900

    Changed store

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,8 @@
 import Initialize from '$lib/Initialize.svelte'
 import Select from '$lib/ui/Select.svelte'
 
+
 </script>
 <div style="width:200px;padding:30px;">
-<Select options="{[{name: 1, value: 1}, {name: 2, value: 2}]}" value="{1}" />
+<Select options="{[{ name: '체크/신용카드', value: 'card'}]}" />
 </div>
