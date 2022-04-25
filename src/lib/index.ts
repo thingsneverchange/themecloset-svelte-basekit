3e71e58646f35c92811bf7f4360e7da7641f6bad
Yongju Lee <lee@yongju.me>
2022-04-25 23:09:31 +0900

    notifcaiton redirect

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,12 @@
 import { store } from './store'
+import type { UiStoreInterface } from './store'
 import Initialize from './Initialize.svelte'
 
 export {
   store,
   Initialize
+}
+
+export type{
+  UiStoreInterface
 }
--- src/lib/ui/Spacer.svelte
+++ src/lib/ui/Spacer.svelte
@@ -1,7 +1,6 @@
 <script type="ts">
 export let spacing: number = 0
 export let spacingHorizontal: number = 0
-export let border: string;
 </script>
 
-<div style="width:{spacing}px; height:{spacingHorizontal}px;border-top:border"></div>
+<div style="width:{spacing}px; height:{spacingHorizontal}px;"></div>
