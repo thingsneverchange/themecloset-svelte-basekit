f4b8c5703c2a41db0109a1ee3783934437adee34
Yongju Lee <lee@yongju.me>
2022-04-29 06:14:30 +0900

    notifcaiton redirect

--- src/lib/ui/Prompt.svelte
+++ src/lib/ui/Prompt.svelte
@@ -42,13 +42,14 @@ const toggle = () :void => {
     </div>
   </div>
   {/if}
-  <div on:click="{toggle}">
+  <div class="promot_trigger" on:click="{toggle}">
     <slot/>
   </div>
 </div>
 <style>
 h3{font-size:11.5pt;padding-bottom:10px;font-weight:600;}
 h4{padding-bottom:10px;opacity:0.8}
+.prompt_container, .promot_trigger{display:inline-block;}
 .prompt{
 	box-shadow:rgba(0, 0, 0, 0.1) 0px 1px 20px 0px;position:fixed;top:20px;left:50%;margin-left:-120px;width:240px;border-radius:7px;padding:20px 25px;font-size:10pt;z-index:999999}
 :global(.theme-light .prompt){background-color:#fff;color:#222;}
