bc50e82d5cc030f260c74cb8923f20f4a1b1e6ae
Yongju Lee <lee@yongju.me>
2022-06-11 15:38:09 +0900

    notifcaiton redirect

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -2,5 +2,5 @@ import { setCookie, getCookies, removeCookie} from './cookies'
 import { _FetchLoad, _FetchPrompt, _FetchClient, _FetchServer} from './fetch'
 
 export {
-  extractCookies, setCookie, getCookies, removeCookie, _FetchLoad, _FetchPrompt, _FetchClient, _FetchServer
+  setCookie, getCookies, removeCookie, _FetchLoad, _FetchPrompt, _FetchClient, _FetchServer
 }
