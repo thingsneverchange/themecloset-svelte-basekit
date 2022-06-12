95cc55bf3c9c9e86f0170072c5663357f53ce75e
Yongju Lee <lee@yongju.me>
2022-06-12 15:08:26 +0900

    notifcaiton redirect

--- src/lib/ui/FlexBox.svelte
+++ src/lib/ui/FlexBox.svelte
@@ -27,5 +27,5 @@ const makeClick = () :void => {
 :global(.fixed-width-3 > *){flex:0 0 33.3333%;}
 :global(.fixed-width-5 > *){flex:0 0 20%;}
 :global(.fixed-width-6 > *){flex:0 0 16.6666%;}
-:global(.flexbox.ellipsis > *{white-space: nowrap;text-overflow:ellipsis;overflow:hidden})
+:global(.flexbox.ellipsis > *){white-space: nowrap;text-overflow:ellipsis;overflow:hidden}
 </style>
