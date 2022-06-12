2de589cbcc5d1e453f1530e57ede094d986ca7fa
Yongju Lee <lee@yongju.me>
2022-06-12 13:41:33 +0900

    notifcaiton redirect

--- src/lib/ui/FlexBox.svelte
+++ src/lib/ui/FlexBox.svelte
@@ -3,6 +3,7 @@ import { createEventDispatcher } from 'svelte';
 export let justifyContent : string = 'initial'
 export let alignItems : string = 'center'
 export let fixedWidth : number = undefined;
+export let ellipsis : boolean = true;
 
 let classNames : string = '';
 export { classNames as class }
@@ -14,7 +15,7 @@ const makeClick = () :void => {
 }
 
 </script>
-<div on:click="{makeClick}" class="{classNames ? classNames : ''} flexbox {fixedWidth ? 'fixed-width-' + fixedWidth : ''}" style="justify-content:{justifyContent}; align-items:{alignItems};">
+<div on:click="{makeClick}" class="{classNames ? classNames : ''} {ellipsis ? ' ellipsis ' : ''} flexbox {fixedWidth ? 'fixed-width-' + fixedWidth : ''}" style="justify-content:{justifyContent}; align-items:{alignItems};">
   <slot/>
 </div>
 
@@ -26,5 +27,5 @@ const makeClick = () :void => {
 :global(.fixed-width-3 > *){flex:0 0 33.3333%;}
 :global(.fixed-width-5 > *){flex:0 0 20%;}
 :global(.fixed-width-6 > *){flex:0 0 16.6666%;}
-:global(.flexbox > *{white-space: nowrap;text-overflow:ellipsis;overflow:hidden})
+:global(.flexbox.ellipsis > *{white-space: nowrap;text-overflow:ellipsis;overflow:hidden})
 </style>
