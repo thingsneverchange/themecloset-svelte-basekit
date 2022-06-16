9862643081298483a2bc8d03809b5a2a011210b8
Yongju Lee <lee@yongju.me>
2022-06-16 14:01:24 +0900

    theme-dark

--- src/lib/ui/Icon.svelte
+++ src/lib/ui/Icon.svelte
@@ -2,16 +2,21 @@
 export let name : string = '';
 export let width : number  = 15;
 export let height : number  = 15;
-export let color : string = '#222'
+export let color : string = null
 
 let iconSvg : string = '';
+let colorNotDefined : boolean = false;
+
+if(color == null){
+  colorNotDefined = true
+}
 
 import(`./images/${name}.svg?raw`).then( (module) => {
   if(!module){
     iconSvg = ''
   }else{
     iconSvg = module.default.replace(/(height=".*?")|(width=".*?")/gi, '')
-    iconSvg = iconSvg.replace('<svg', `<svg fill="${color}" width="${width}px" height="${height}px"`)
+    iconSvg = iconSvg.replace('<svg', `<svg ${colorNotDefined ? 'colorNotDefined="true"' : ''} fill="${color}" width="${width}px" height="${height}px"`)
   }
 })
 </script>
@@ -19,6 +24,7 @@ import(`./images/${name}.svg?raw`).then( (module) => {
 {@html iconSvg}
 
 <style>
-
+.theme-dark svg[colorNotDefined=true]{fill:#fff}
+.theme-light svg[colorNotDefined=true]{fill:#222}
 :global(svg){padding:0px;margin:0px;box-sizing:border-box;}
 </style>
